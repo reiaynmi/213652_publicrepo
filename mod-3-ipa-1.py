5042897f849add2e645ccd6d3cb4eef2bdf13ece
reiaynmi <rockymacumbal@gmail.com>
2022-07-26 12:55:33 +0800

    upload in correct format

---
+++ mod-3-ipa-1.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+'''Module 3: Individual Programming Assignment 1
+Thinking Like a Programmer
+This assignment covers your intermediate proficiency with Python.
+'''
+
+def shift_letter(letter, shift):
+    '''Shift Letter. 
+    5 points.
+    
+    Shift a letter right by the given number.
+    Wrap the letter around if it reaches the end of the alphabet.
+    Examples:
+    shift_letter("A", 0) -> "A"
+    shift_letter("A", 2) -> "C"
+    shift_letter("Z", 1) -> "A"
+    shift_letter("X", 5) -> "C"
+    shift_letter(" ", _) -> " "
+    *Note: the single underscore `_` is used to acknowledge the presence
+        of a value without caring about its contents.
+    Parameters
+    ----------
+    letter: str
+        a single uppercase English letter, or a space.
+    shift: int
+        the number by which to shift the letter. 
+    Returns
+    -------
+    str
+        the letter, shifted appropriately, if a letter.
+        a single space if the original letter was a space.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    #return space if space
+    if letter == " ":
+        return (" ")
+    else:
+        order_letter = ord(letter) + int(shift)
+        #if chr goes past 90 (i.e goes past Z), reset and subtract 26 to return to A
+        if order_letter > 90:
+            order_letter -= 26
+            return (chr(order_letter))
+        else:
+            return (chr(order_letter))
+
+def caesar_cipher(message, shift):
+    '''Caesar Cipher. 
+    10 points.
+    
+    Apply a shift number to a string of uppercase English letters and spaces.
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and spaces.
+    shift: int
+        the number by which to shift the letters. 
+    Returns
+    -------
+    str
+        the message, shifted appropriately.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    #initialize cipher list. iterate over message to get needed characters
+    cipher = []
+    for x in message:
+        if x == " ": #return space if space
+            cipher.append(" ")
+        else:
+            order_letter = ord(x) + int(shift)
+            if ((order_letter > 90) and (ord(x) < 97)) or (order_letter > 122): #check for small letter cases
+                order_letter -= 26
+                cipher.append(chr(order_letter))
+            else:
+                cipher.append(chr(order_letter))
+    cipher_string = '' #initalize cipher string
+    for y in cipher: #iterate over list
+        cipher_string += y
+    return cipher_string #return
+
+def shift_by_letter(letter, letter_shift):
+    '''Shift By Letter. 
+    10 points.
+    
+    Shift a letter to the right using the number equivalent of another letter.
+    The shift letter is any letter from A to Z, where A represents 0, B represents 1, 
+        ..., Z represents 25.
+    Examples:
+    shift_by_letter("A", "A") -> "A"
+    shift_by_letter("A", "C") -> "C"
+    shift_by_letter("B", "K") -> "L"
+    shift_by_letter(" ", _) -> " "
+    Parameters
+    ----------
+    letter: str
+        a single uppercase English letter, or a space.
+    letter_shift: str
+        a single uppercase English letter.
+    Returns
+    -------
+    str
+        the letter, shifted appropriately.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    #same process as shift_letter. subtract order of letter_shift by 65 to get desired results
+    if letter == " ":
+        print (" ")
+    else:
+        order_letter = ord(letter.capitalize()) + ord(letter_shift) - 65
+        if order_letter > 90:
+            order_letter -= 26
+            return (chr(order_letter))
+        else:
+            return (chr(order_letter))
+
+def vigenere_cipher(message, key):
+    '''Vigenere Cipher. 
+    15 points.
+    
+    Encrypts a message using a keyphrase instead of a static number.
+    Every letter in the message is shifted by the number represented by the 
+        respective letter in the key.
+    Spaces should be ignored.
+    Example:
+    vigenere_cipher("A C", "KEY") -> "K A"
+    If needed, the keyphrase is extended to match the length of the key.
+        If the key is "KEY" and the message is "LONGTEXT",
+        the key will be extended to be "KEYKEYKE".
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and spaces.
+    key: str
+        a string of uppercase English letters. Will never be longer than the message.
+        Will never contain spaces.
+    Returns
+    -------
+    str
+        the message, shifted appropriately.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    cipher = []
+    key_list = list(key) #initialize key as list
+    key_count = 0 
+    for x in message: #iterate over all characters of message, and all characters of key
+        if x == " ":
+            cipher.append(" ")
+            key_count +=1
+        else:
+            order_letter = ord(x) + ord(key_list[key_count]) - 65 
+            if ((order_letter > 90) and (ord(x) < 97)) or (order_letter > 122): 
+                order_letter -= 26
+                cipher.append(chr(order_letter))
+                key_count +=1 
+            else:
+                cipher.append(chr(order_letter))
+                key_count+=1
+    cipher_string = ""
+    for y in cipher:
+        cipher_string += y
+    return cipher_string
+
+def scytale_cipher(message, shift):
+    '''Scytale Cipher.
+    15 points.
+    
+    Encrypts a message by simulating a scytale cipher.
+    A scytale is a cylinder around which you can wrap a long strip of 
+        parchment that contained a string of apparent gibberish. The parchment, 
+        when read using the scytale, would reveal a message due to every nth 
+        letter now appearing next to each other, revealing a proper message.
+    This encryption method is obsolete and should never be used to encrypt
+        data in production settings.
+    You may read more about the method here:
+        https://en.wikipedia.org/wiki/Scytale
+    You may follow this algorithm to implement a scytale-style cipher:
+    1. Take a message to be encoded and a "shift" number. 
+        For this example, we will use "INFORMATION_AGE" as 
+        the message and 3 as the shift.
+    2. Check if the length of the message is a multiple of 
+        the shift. If it is not, add additional underscores 
+        to the end of the message until it is. 
+        In this example, "INFORMATION_AGE" is already a multiple of 3, 
+        so we will leave it alone.
+    3. This is the tricky part. Construct the encoded message. 
+        For each index i in the encoded message, use the character at the index
+        (i // shift) + (len(message) // shift) * (i % shift) of the raw message. 
+        If this number doesn't make sense, you can play around with the cipher at
+         https://dencode.com/en/cipher/scytale to try to understand it.
+    4. Return the encoded message. In this case, 
+        the output should be "IMNNA_FTAOIGROE".
+    Example:
+    scytale_cipher("INFORMATION_AGE", 3) -> "IMNNA_FTAOIGROE"
+    scytale_cipher("INFORMATION_AGE", 4) -> "IRIANMOGFANEOT__"
+    scytale_cipher("ALGORITHMS_ARE_IMPORTANT", 8) -> "AOTSRIOALRH_EMRNGIMA_PTT"
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and underscores (underscores represent spaces)
+    shift: int
+        a positive int that does not exceed the length of message
+    Returns
+    -------
+    str
+        the encoded message
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    cipher = []
+    underscores = len(message) % shift #length of needed underscores
+    while underscores % shift !=0: #terminate when length of needed underscores is met
+        message += "_"
+        underscores +=1
+    counter = 0
+    while counter < len(message): #loop, iterate len(message) times
+        scytale_index = (counter // shift) + ((len(message) // shift) * (counter % shift))
+        cipher.append(message[scytale_index]) #new index
+        counter +=1
+    cipher_string = '' 
+    for y in cipher:
+        cipher_string += y
+    return cipher_string
+
+def scytale_decipher(message, shift):
+    '''Scytale De-cipher.
+    15 points.
+    Decrypts a message that was originally encrypted with the `scytale_cipher` function above.
+    Example:
+    scytale_decipher("IMNNA_FTAOIGROE", 3) -> "INFORMATION_AGE"
+    scytale_decipher("AOTSRIOALRH_EMRNGIMA_PTT", 8) -> "ALGORITHMS_ARE_IMPORTANT"
+    scytale_decipher("IRIANMOGFANEOT__", 4) -> "INFORMATION_AGE_"
+    There is no further brief for this number.
+    
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and underscores (underscores represent spaces)
+    shift: int
+        a positive int that does not exceed the length of message
+    Returns
+    -------
+    str
+        the decoded message
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    return scytale_cipher(message,(len(message) // shift)) #call scytale_cipher to unwind 
+
---
+++ mod-4-ipa-1.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#sample data
+social_graph = {
+    "@bongolpoc":{"first_name":"Joselito",
+                  "last_name":"Olpoc",
+                  "following":[
+                  ]
+    },
+    "@joaquin":  {"first_name":"Joaquin",
+                  "last_name":"Gonzales",
+                  "following":[
+                      "@chums","@jobenilagan"
+                  ]
+    },
+    "@chums" : {"first_name":"Matthew",
+                "last_name":"Uy",
+                "following":[
+                    "@bongolpoc","@miketan","@rudyang","@joeilagan"
+                ]
+    },
+    "@jobenilagan":{"first_name":"Joben",
+                   "last_name":"Ilagan",
+                   "following":[
+                    "@eeebeee","@joeilagan","@chums","@joaquin"
+                   ]
+    },
+    "@joeilagan":{"first_name":"Joe",
+                  "last_name":"Ilagan",
+                  "following":[
+                    "@eeebeee","@jobenilagan","@chums"
+                  ]
+    },
+    "@eeebeee":  {"first_name":"Elizabeth",
+                  "last_name":"Ilagan",
+                  "following":[
+                    "@jobenilagan","@joeilagan"
+                  ]
+    },
+}
+board1 = [
+['X','X','O'],
+['O','X','O'],
+['O','','X'],
+]
+
+board2 = [
+['X','X','O'],
+['O','X','O'],
+['','O','X'],
+]
+
+board3 = [
+['O','X','O'],
+['','O','X'],
+['X','X','O'],
+]
+
+board4 = [
+['X','X','X'],
+['O','X','O'],
+['O','','O'],
+]
+
+board5 = [
+['X','X','O'],
+['O','X','O'],
+['X','','O'],
+]
+
+board6 = [
+['X','X','O'],
+['O','X','O'],
+['X','',''],
+]
+
+board7 = [
+['X','X','O',''],
+['O','X','O','O'],
+['X','','','O'],
+['O','X','','']
+]
+legs = {
+     ("upd","admu"):{
+         "travel_time_mins":10
+     },
+     ("admu","dlsu"):{
+         "travel_time_mins":35
+     },
+     ("dlsu","upd"):{
+         "travel_time_mins":55
+     }
+}
+
+legs1 = {
+    ('a1', 'a2'): {
+        'travel_time_mins': 10
+    },
+    ('a2', 'b1'): {
+        'travel_time_mins': 10230
+    },
+    ('b1', 'a1'): {
+        'travel_time_mins': 1
+    }
+}
+
+
+# In[ ]:
+
+
+'''Module 4: Individual Programming Assignment 1
+Parsing Data
+This assignment covers your ability to manipulate data in Python.
+'''
+
+def relationship_status(from_member, to_member, social_graph):
+    '''Relationship Status.
+    20 points.
+    Let us pretend that you are building a new app.
+    Your app supports social media functionality, which means that users can have
+    relationships with other users.
+    There are two guidelines for describing relationships on this social media app:
+    1. Any user can follow any other user.
+    2. If two users follow each other, they are considered friends.
+    This function describes the relationship that two users have with each other.
+    Please see "assignment-4-sample-data.py" for sample data. The social graph
+    will adhere to the same pattern.
+    Parameters
+    ----------
+    from_member: str
+        the subject member
+    to_member: str
+        the object member
+    social_graph: dict
+        the relationship data    
+    Returns
+    -------
+    str
+        "follower" if fromMember follows toMember,
+        "followed by" if fromMember is followed by toMember,
+        "friends" if fromMember and toMember follow each other,
+        "no relationship" if neither fromMember nor toMember follow each other.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    status = "No relationship"
+    following = False
+    followedby = False
+    try: #try if from_member exists
+        if to_member in social_graph[from_member]["following"]:
+            following = True
+    except: #return error if from_member does not exist
+        status = "Error: member(s) not found in social graph"
+    try: #try if to_member is in social graph
+        if from_member in social_graph[to_member]["following"]:
+            followedby = True
+    except: #handle error if to_member is not in social graph but is followed by from_member (who has a social graph
+        if following == True:
+            status = "Following"
+    #outcomes
+    if following == True and followedby == True:
+        status = "Friends"
+    elif following == True and followedby == False:
+        status = "Following"
+    elif following == False and followedby == True:
+        status = "Followed by"
+    return status
+
+
+def tic_tac_toe(board):
+    '''Tic Tac Toe. 
+    25 points.
+    Tic Tac Toe is a common paper-and-pencil game. 
+    Players must attempt to successfully draw a straight line of their symbol across a grid.
+    The player that does this first is considered the winner.
+    This function evaluates a tic tac toe board and returns the winner.
+    Please see "assignment-4-sample-data.py" for sample data. The board will adhere
+    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
+    have more than one winner. The board will only ever have 2 unique symbols at the same time.
+    Parameters
+    ----------
+    board: list
+        the representation of the tic-tac-toe board as a square list of lists
+    Returns
+    -------
+    str
+        the symbol of the winner or "NO WINNER" if there is no winner
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    #default case
+    status = False
+    i = 0
+    #list of all vertical cases
+    vert = [list(x) for x in zip(*board)]
+    #diagonal up case ([0][0], [1][1], ... [i], [i]). convert to set to check number of unique elements
+    diagonalup_set = set([board[i][i] for i,v in enumerate(board)])
+    #diagonal down case ([0][2], [1][1], ... [i], [len(board) - i -1]). convert to set to check numebr of unique elements
+    diagonaldown_set = set([board[i][len(board)-i-1] for i,v in enumerate(board)])
+    #iterate for a max of len(board) times until true
+    while i < len(board):
+        hor_set = set(board[i])
+        vert_set = set(vert[i])
+        #check if tic tac toe is achieved (only one unique element)
+        if len(hor_set)== 1 or len(vert_set) == 1 or len(diagonalup_set) == 1 or len(diagonaldown_set) == 1 :
+            status = True
+            #stop loop
+            i = len(board)
+        else:
+            i+=1
+    return status
+
+def eta(first_stop, second_stop, route_map):
+    '''ETA. 
+    25 points.
+    A shuttle van service is tasked to travel along a predefined circlar route.
+    This route is divided into several legs between stops.
+    The route is one-way only, and it is fully connected to itself.
+    This function returns how long it will take the shuttle to arrive at a stop
+    after leaving another stop.
+    Please see "mod-4-ipa-1-sample-data.py" for sample data. The route map will
+    adhere to the same pattern. The route map may contain more legs and more stops,
+    but it will always be one-way and fully enclosed.
+    Parameters
+    ----------
+    first_stop: str
+        the stop that the shuttle will leave
+    second_stop: str
+        the stop that the shuttle will arrive at
+    route_map: dict
+        the data describing the routes
+    Returns
+    -------
+    int
+        the time it will take the shuttle to travel from first_stop to second_stop
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    #list of keys
+    legs_keys= list(route_map.keys())
+    total = 0
+    if (first_stop, second_stop) in legs_keys:
+        #recursion function, root case
+        return route_map[(first_stop, second_stop)]['travel_time_mins']
+    else:
+        i = 0
+        #iterate for a max of len(route_map) times until root case is achieved
+        while i < len(route_map):
+            if second_stop in legs_keys[i]:
+                if legs_keys[i][0] == second_stop: #second_stop has to be the second stop when searching for routes
+                    i+=1
+                else:
+                    intermediary = legs_keys[i][0]
+                    total += eta(first_stop, intermediary, route_map) #recursion
+                    total += eta(intermediary, second_stop, route_map) #recursion
+                    i = len(route_map)
+            else: 
+                i +=1 
+    return total
+
+
+# In[ ]:
+
+
+
+
